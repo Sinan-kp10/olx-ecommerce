0b5e5e6392229f8e5c1dad4c094797ef73eb6b48
Sinan-kp10 <sinankp9503@gmail.com>
2026-08-30 12:31:58 +0530

    fix vercel routing

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,7 +12,7 @@ app.use(express.json())
 
 app.use(
     cors({
-        origin: ["http://localhost:5173","https://olx-ecommerce-clone.vercel.app/"],
+        origin: ["http://localhost:5173","https://olx-ecommerce-clone-5yg4.vercel.app"],
         credentials: true
     })
 )
