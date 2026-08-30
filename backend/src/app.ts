00123755b6fcce7e387390a7bee6e5daf0c7ff47
Sinan-kp10 <sinankp9503@gmail.com>
2026-08-30 11:25:16 +0530

    fix : Vercel build

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,7 +12,7 @@ app.use(express.json())
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://olx-ecommerce-clone.vercel.app",
         credentials: true
     })
 )
